55d71469c4434ac2f733ccd1816728470eac1a71
Inferato <Infaustmager@gmail.com>
2020-09-14 01:28:44 +0300

    some fix

--- FLoan/Loan/views.py
+++ FLoan/Loan/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from .models import Borrower, Loan, Fee, Payments, CreditProduct
